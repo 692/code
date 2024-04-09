db53b4e6fdde3f7a2fe0304366bf74b6179b9163
692 <6889692@gmail.com>
2024-04-09 23:20:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'fd.6889692.onflashdrive.app'
+      url.hostname = 'ts.6889692.onflashdrive.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
